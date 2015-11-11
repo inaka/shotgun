inaka/shotgun

commit 0243a27304faf0c58eff74103757eaa2229a37b7

[#36] Return the server response when a chunked connection cannot be established.

src/shotgun.erl
@@ -310,14 +310,13 @@ at_rest({get_async, {HandleEvent, AsyncMode}, Args}, From, #{pid := Pid}) ->
     StreamRef = do_http_verb(get, Pid, Args),
     CleanState = clean_state(),
     NewState = CleanState#{
+                 from => From,
                  pid => Pid,
                  stream => StreamRef,
                  handle_event => HandleEvent,
                  async => true,
                  async_mode => AsyncMode
                 },
-    Result = {ok, StreamRef},
-    gen_fsm:reply(From, Result),
     {next_state, wait_response, NewState};
 at_rest({HttpVerb, Args}, From, #{pid := Pid}) ->
     StreamRef = do_http_verb(HttpVerb, Pid, Args),
@@ -344,13 +343,15 @@ wait_response({gun_response, _Pid, _StreamRef, fin, StatusCode, Headers},
                 gen_fsm:reply(From, {ok, Response}),
                 Responses;
             true ->
-                queue:in(Response, Responses)
+                gen_fsm:reply(From, {ok, Response})
         end,
     {next_state, at_rest, State#{responses => NewResponses}};
 wait_response({gun_response, _Pid, _StreamRef, nofin, StatusCode, Headers},
-              State) ->
+              #{from := From, stream := StreamRef} = State) ->
     StateName = case lists:keyfind(<<"transfer-encoding">>, 1, Headers) of
                     {<<"transfer-encoding">>, <<"chunked">>} ->
+                        Result = {ok, StreamRef},
+                        gen_fsm:reply(From, Result),
                         receive_chunk;
                     _ ->
                         receive_data

commit 7423e161d8ad14c119cb3aefc0762fc918ad9948

Maybe fixes Issue #95
Some spelunking leads me to believe that commit
0243a27304faf0c58eff74103757eaa2229a37b7
accidentally replaced a queue append operation
with an assignment of the return value of
gen_fsm:reply/2, (whose return value you are
supposed to ignore.). This value was then assigned
to the responses key in the FSM's state map,
which was only *supposed* to contain data of
type queue:queue().

This commit restores the queue append operation,
and *appears* to address Issue #95.

src/shotgun.erl
@@ -471,7 +471,8 @@ wait_response({gun_response, _Pid, _StreamRef, fin, StatusCode, Headers},
                 gen_fsm:reply(From, {ok, Response}),
                 Responses;
             true ->
-                gen_fsm:reply(From, {ok, Response})
+                gen_fsm:reply(From, {ok, Response}),
+                queue:in(Response, Responses)
         end,
     {next_state, at_rest, State#{responses => NewResponses}, 0};
 wait_response({gun_response, _Pid, _StreamRef, nofin, StatusCode, Headers},