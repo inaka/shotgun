034cc00b580e79b73452db95cab51ff1ea0cfdfc
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2025-08-07 23:41:23 -0700

    Add feature to allow disabling reconnection. (#203)
    * Add feature to allow disabling reconnection.
    
    * Address PR feedback.
    
    * Add newline to end of test file.

--- test/shotgun_async_SUITE.erl
+++ test/shotgun_async_SUITE.erl
@@ -2,7 +2,15 @@
 
 -export([all/0, init_per_suite/1, end_per_suite/1, init_per_testcase/2,
          end_per_testcase/2]).
--export([get_sse/1, get_binary/1, work_queue/1, get_handle_event/1, async_unsupported/1]).
+-export([
+    get_sse/1,
+    get_binary/1,
+    work_queue/1,
+    get_handle_event/1,
+    async_unsupported/1,
+    async_gun_down_no_reopen/1,
+    async_gun_down_with_reopen/1
+]).
 
 -include_lib("common_test/include/ct.hrl").
 
@@ -161,5 +169,88 @@ async_unsupported(Config) ->
 
     ct:comment("Async PUT should return an error"),
     {error, {async_unsupported, put}} = shotgun:put(Conn, "/", #{}, <<>>, #{async => true}),
+
+    {comment, ""}.
+
+-spec async_gun_down_no_reopen(shotgun_test_utils:config()) -> {comment, string()}.
+async_gun_down_no_reopen(Config) ->
+    Conn = ?config(conn, Config),
+    Self = self(),
+
+    ct:comment("SSE: GET should return a ref"),
+    HandleEvent =
+        fun(_, _, EventBin) ->
+           case shotgun:parse_event(EventBin) of
+               #{id := Data} ->
+                   Self ! Data;
+               _ ->
+                   ok
+           end
+        end,
+    Opts =
+        #{async => true,
+          async_mode => sse,
+          allow_reconnect => false,
+          handle_event => HandleEvent},
+    {ok, _Ref} = shotgun:get(Conn, <<"/chunked-sse/3">>, #{}, Opts),
+
+    timer:sleep(500),
+
+    ok = shotgun_test_utils:wait_receive(<<"1">>, 500),
+    {at_rest, _} = sys:get_state(Conn),
+    %% Server is stopped immediately after receiving event.
+    %% We should be at rest waiting for the next event.
+    http_server:stop_listener(),
+    timer:sleep(500),
+    %% Listener needs to be running at test end.
+    http_server:start_listener(),
+
+    ct:comment("Connection should be closed after server shutdown"),
+    false = erlang:is_process_alive(Conn),
+
+    {comment, ""}.
+
+-spec async_gun_down_with_reopen(shotgun_test_utils:config()) -> {comment, string()}.
+async_gun_down_with_reopen(Config) ->
+    Conn = ?config(conn, Config),
+    Self = self(),
+
+    ct:comment("SSE: GET should return a ref"),
+    HandleEvent =
+        fun(_, _, EventBin) ->
+           case shotgun:parse_event(EventBin) of
+               #{id := Data} ->
+                   Self ! Data;
+               _ ->
+                   ok
+           end
+        end,
+    Opts =
+        #{async => true,
+          async_mode => sse,
+          allow_reconnect => true,
+          handle_event => HandleEvent},
+    {ok, _Ref} = shotgun:get(Conn, <<"/chunked-sse/3">>, #{}, Opts),
+
+    timer:sleep(500),
+
+    ok = shotgun_test_utils:wait_receive(<<"1">>, 500),
+    {at_rest, _} = sys:get_state(Conn),
+    %% Server is stopped immediately after receiving event.
+    %% We should be at rest waiting for the next event.
+    http_server:stop_listener(),
+    timer:sleep(500),
+
+    %% Listener needs to be running at test end.
+    http_server:start_listener(),
+
+    ct:comment("Connection should still be alive"),
+    true = erlang:is_process_alive(Conn),
+
+    Port = application:get_env(http_server, http_port, 8888),
+    shotgun:reopen(Conn, "localhost", Port),
+
+    {ok, _Ref2} = shotgun:get(Conn, <<"/chunked-sse/3">>, #{}, Opts),
+    ok = shotgun_test_utils:wait_receive(<<"1">>, 500),
 
     {comment, ""}.
